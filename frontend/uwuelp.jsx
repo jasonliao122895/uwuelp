58fe9a7195f9912ebd99cb7f724afa09af8fd355
Jason Liao <jasonliao@live.com>
2019-12-27 13:55:23 -0800

    removed logger

--- frontend/uwuelp.jsx
+++ frontend/uwuelp.jsx
@@ -24,8 +24,8 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   
 
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
 
   ReactDOM.render(
     <Root store={store}/>,
